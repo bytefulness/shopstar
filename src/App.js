28f16444346be41afc54f20fe7bb2116bb07b5cd
eakyel <e.akyel@outlook.com>
2021-05-21 23:02:29 +0300

    Fetch products from the store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerse";
 import { Navbar, Products } from "./components";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // Fetch products from the api
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <Navbar />
-      <Products />
+      <Products products={products} />
     </>
   );
 }
