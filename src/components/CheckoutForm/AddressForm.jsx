524e58aa8153ace70a1bff79f3b845f265744bf8
eakyel <e.akyel@outlook.com>
2021-05-29 23:15:20 +0300

    Fetch shipping countries from API and render

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -13,12 +13,17 @@ import FormInput from "./CustomTextField";
 
 const AddressForm = ({ checkoutToken }) => {
   const [shippingCountries, setShippingCountries] = useState([]);
-  const [shippingCounter, setShippingCountry] = useState("");
+  const [shippingCountry, setShippingCountry] = useState("");
   const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
   const [shippingSubdivision, setShippingSubdivision] = useState("");
   const [shippingOptions, setShippingOptions] = useState([]);
   const [shippingOption, setShippingOption] = useState("");
   const methods = useForm();
+
+  const countries = Object.entries(shippingCountries).map(([code, name]) => {
+    return { id: code, label: name };
+  });
+  console.log(countries);
 
   const fetchShippingCountries = async (checkoutTokenId) => {
     try {
@@ -51,16 +56,23 @@ const AddressForm = ({ checkoutToken }) => {
             <FormInput required name="email" label="Email" />
             <FormInput required name="zip" label="ZIP / Postal code" />
 
-            {/* <Grid item xs={12} sm={6}>
-            <InputLabel>Shipping Country</InputLabel>
-            <Select value={ } fullWitdh onChange={ }>
-              <MenuItem key={ } value={ }>
-                Select Me
-              </MenuItem>
-            </Select>
-          </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
+              <Select
+                value={shippingCountry}
+                fullWitdh
+                onChange={(e) => setShippingCountry(e.target.value)}
+              >
+                {countries.map((item) => (
+                  <MenuItem key={item.id} value={item.id}>
+                    {item.label}
+                  </MenuItem>
+                ))}
+                {/*  */}
+              </Select>
+            </Grid>
 
-          <Grid item xs={12} sm={6}>
+            {/* <Grid item xs={12} sm={6}>
             <InputLabel>Shipping Country</InputLabel>
             <Select value={ } fullWitdh onChange={ }>
               <MenuItem key={ } value={ }>
